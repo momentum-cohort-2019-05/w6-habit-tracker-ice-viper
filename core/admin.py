f247fbb86bf36df0220b7c7e78064f1b29a5f3f6
dwcope3 <dwcope3@gmail.com>
2019-07-01 13:12:53 -0400

    register models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from core.models import Habit, Record
 
 # Register your models here.
+admin.site.register(Habit)
+admin.site.register(Record)
+
 class HabitAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("user", "name")}
+
+
 
