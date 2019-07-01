3673065389af386cfdebb3865905e3d41d05e96e
dwcope3 <dwcope3@gmail.com>
2019-07-01 16:57:35 -0400

    change/update models

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from core.models import Habit, Record
+from core.models import Habit, Daily_Record
 
 # Register your models here.
 admin.site.register(Habit)
-admin.site.register(Record)
+admin.site.register(Daily_Record)
 
 # class HabitAdmin(admin.ModelAdmin):
 #     prepopulated_fields = {"slug": ("name",)}
--- core/models.py
+++ core/models.py
@@ -4,22 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Habit(models.Model):
     name = models.CharField(max_length=100)
-    daily_goal = models.PositiveIntegerField(default=0)
+    habit_goal = models.PositiveIntegerField(default=0)
     #True habit_types are positive habits
     habit_type = models.BooleanField(default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # slug = models.SlugField(name, max_length=100, unique=True, null=True)
-    date_started = models.DateField(auto_now_add=True)
+    # date_started = models.DateField(auto_now_add=True)
+    units = models.CharField(max_length=30,)
 
     def __str__(self):
         return self.name
 
 
-class Record(models.Model):
+class Daily_Record(models.Model):
     num_actions = models.PositiveIntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    record_date = models.DateField(auto_now_add=True)
+    created_on = models.DateField(auto_now_add=True)
     habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.record_date}, {self.habit}'
+        return f'{self.created_on}, {self.habit}'
