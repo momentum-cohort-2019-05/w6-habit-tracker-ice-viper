e108a8476a74628abc5129477e43fc1103fa49a9
Zeke Hart <heirloch@gmail.com>
2019-07-02 12:00:28 -0400

    adds user filtering to habit list page

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from core.models import Habit, DailyRecord
 from django.contrib.auth.decorators import login_required
-from django.db.models import Max
+from django.db.models import Max, Q
 
 
 # Create your views here.
@@ -12,8 +12,8 @@ def welcome_page(request):
 
 @login_required
 def all_habits(request):
-    habits_list = Habit.objects.annotate(
-        latest_record=Max('dailyrecord__created_on'))
+    habits_list = Habit.objects.annotate(latest_record=Max(
+        'dailyrecord__created_on', filter=Q(user=request.user)))
 
     return render(request, "user_habits.html", {
         'habits_list': habits_list,
