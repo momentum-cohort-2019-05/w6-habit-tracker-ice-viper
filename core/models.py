72a68fc006fcaf00a287dcd953c755d6fccc44dc
Zeke Hart <heirloch@gmail.com>
2019-07-02 11:58:18 -0400

    annotation works but does not filter by user

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
+
 # from django.utils import timezone
 
 
@@ -13,10 +14,11 @@ class Habit(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # slug = models.SlugField(name, max_length=100, unique=True, null=True)
     # date_started = models.DateField(auto_now_add=True)
-    units = models.CharField(max_length=30,)
-    
+    units = models.CharField(max_length=30, )
+
     # def get_most_recent_record(self):
-    #     most_recent_record = self.DailyRecord.get(pk=1)
+    #     ads = self.dailyrecord_set.order_by("-created_on")[0]
+    #     return ads.created_on
 
     # def __str__(self):
     #     return self.name
