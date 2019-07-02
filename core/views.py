ee5e178890a23e0da1e9945b00add25e53db7f2c
dwcope3 <dwcope3@gmail.com>
2019-07-02 13:43:07 -0400

    create new-dailyrecord form

--- core/views.py
+++ core/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from core.models import Habit, DailyRecord
 from django.contrib.auth.decorators import login_required
 from django.db.models import Max, Q
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse
 
 
 # Create your views here.
@@ -18,3 +21,39 @@ def all_habits(request):
     return render(request, "user_habits.html", {
         'habits_list': habits_list,
     })
+
+
+
+class DailyRecordCreate(LoginRequiredMixin, CreateView):
+    """
+    Form for adding a blog comment. Requires login. 
+    """
+    model = DailyRecord
+    fields = ['description',]
+
+    def get_context_data(self, **kwargs):
+        """
+        Add associated blog to form template so can display its title in HTML.
+        """
+        # Call the base implementation first to get a context
+        context = super(BlogCommentCreate, self).get_context_data(**kwargs)
+        # Get the blog from id and add it to the context
+        context['blog'] = get_object_or_404(Blog, pk = self.kwargs['pk'])
+        return context
+        
+    def form_valid(self, form):
+        """
+        Add author and associated blog to form data before setting it as valid (so it is saved to model)
+        """
+        #Add logged-in user as author of comment
+        form.instance.author = self.request.user
+        #Associate comment with blog based on passed id
+        form.instance.blog=get_object_or_404(Blog, pk = self.kwargs['pk'])
+        # Call super-class form validation behaviour
+        return super(BlogCommentCreate, self).form_valid(form)
+
+    def get_success_url(self): 
+        """
+        After posting comment return to associated blog.
+        """
+        return reverse('blog-detail', kwargs={'pk': self.kwargs['pk'],})
